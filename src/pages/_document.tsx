5f14625b60266235ea6b6a68a414355d3995dcef
pedrohrocha18 <phr.tech@outlook.com>
2024-02-03 08:19:16 -0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <title>Rochas e-commerce</title>
+      <title>Rochas e-commercee</title>
       <link rel="shortcut icon" href="/icon.svg" type="image/x-icon" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
